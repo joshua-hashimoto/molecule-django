d98b932d16c2ba7bbc47d4d369687f3d4087f67c
dropscar <dropscar041205@gmail.com>
2020-08-25 13:43:49 +0900

    separate config to dedicated files

---
+++ core/configs/message_framework.py
@@ -0,0 +1,7 @@
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.INFO: "uk-alert-primary",
+    messages.SUCCESS: "uk-alert-success",
+    messages.WARNING: "uk-alert-warning",
+    messages.ERROR: "uk-alert-danger",
+}
