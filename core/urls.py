2359ccc101b1f2161d89e360db02e841ac63546e
dropscar <dropscar041205@gmail.com>
2021-01-07 01:56:49 +0900

    update static and media url for develop

--- core/urls.py
+++ core/urls.py
@@ -18,9 +18,13 @@ urlpatterns = [
     path('comments/', include('comments.urls')),
     path('tags/', include('tags.urls')),
     path('', include('articles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
